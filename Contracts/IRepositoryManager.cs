cca35135fc199ed1c959cb0a2ed3376d2c65d566
Claes Engelin <claes.engelin@zocom.se>
2022-05-12 14:41:35 +0200

    Efter avsnitt 3.7
    Gjort klart Repository Manager

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Contracts;
+public interface IRepositoryManager
+{
+    public ICompanyRepository Company { get; }
+    public IEmployeeRepository Employee { get; }
+
+    void Save();
+}
