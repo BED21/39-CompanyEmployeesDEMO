42465f862784a5ddfafeb4c60373d95af12df0f2
Claes Engelin <claes.engelin@zocom.se>
2022-05-18 09:47:07 +0200

    Efter kapitel 3
    Implementerat en komplett Onion Architecture, med Repsoitoris´es och Services

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -3,6 +3,8 @@ using Contracts;
 
 using LoggerService;
 
+using Microsoft.EntityFrameworkCore;
+
 using Repository;
 
 using Service;
@@ -34,4 +36,9 @@ public static class ServiceExtensions
 
     public static void ConfigureServiceManager(this IServiceCollection services) =>
         services.AddScoped<IServiceManager, ServiceManager>();
+
+    public static void ConfigureSqlContext(this IServiceCollection services,
+        IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 }
