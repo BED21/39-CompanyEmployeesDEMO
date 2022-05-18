18f57bd8db7b4a4243a9124ccab99429f7794e4e
Claes Engelin <claes.engelin@zocom.se>
2022-05-18 13:08:38 +0200

    Efter kapitel 4
    Lagt till Service Layer, AutoMap och gjort en första GET-metod

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -6,8 +6,13 @@ using Entities.Models;
 namespace Repository;
 public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
 {
-    public CompanyRepository(RepositoryContext repositoryContext) 
+    public CompanyRepository(RepositoryContext repositoryContext)
         : base(repositoryContext)
     {
     }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
 }
