a584ca21c07749540f2852fdbb9275d220b674d8
Claes Engelin <claes.engelin@zocom.se>
2022-05-25 09:56:35 +0200

    Efter kapitel 9

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,6 @@
 ﻿
+using CompanyEmployees.Presentation.ModelBinders;
+
 using Microsoft.AspNetCore.Mvc;
 
 using Service.Contracts;
@@ -33,7 +35,7 @@ public class CompaniesController : ControllerBase
     }
 
     [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-    public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+    public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
     {
         var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
         return Ok(companies);
